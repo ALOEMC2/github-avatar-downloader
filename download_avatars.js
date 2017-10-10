030bf228db5edad0a7f772c361ade8ca07f0ec84
Al OFlaherty <aloemc2@gmail.com>
2017-10-10 11:21:15 +0000

    request address completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,16 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-
+var repoOwner = 'jquery';
+var repoName = 'jquery';
+var GITHUB_USER = "ALOEMC2";
+var GITHUB_TOKEN = "0eef002613ca18cfdc98f35d213007e887787c6a";
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log (requestURL)
 function getRepoContributors(repoOwner, repoName, cb) {
-  getRepoContributors("nodejs", "node", function(err, result) {
+  getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
 }
+//}https://api.github.com/repos/jquery/jquery/contributors
