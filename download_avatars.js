09d821623f89b2b548b741a695a8bc347745feda
Al OFlaherty <aloemc2@gmail.com>
2017-10-10 02:08:50 +0000

    nodejs node

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,7 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  getRepoContributors("jquery", "jquery", function(err, result) {
+  getRepoContributors("nodejs", "node", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
